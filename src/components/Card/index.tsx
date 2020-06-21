908dd9d8c6786bc1d495993ca8b96509656b8d6b
rauzola <raul_sigoli@hotmail.com>
2020-06-21 19:12:37 -0300

    Card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Container, Label } from "./styles";
+
+export default function index() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1" />
+      </header>
+      <p>Fazer a migração completa de servidor</p>
+      <img
+        src="https://avatars3.githubusercontent.com/u/57502280?s=460&u=a45f9f60b5440cc8364a36880830cf69b74ecdd2&v=4"
+        alt="raul"
+      />
+    </Container>
+  );
+}
